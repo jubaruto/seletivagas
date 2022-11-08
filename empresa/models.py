7cba58ce63b1499b5f8ad5884f83dfe662c7469b
Juliana Souto <barutotec@gmail.com>
2022-11-08 09:48:32 -0300

    Foi inserido os códigos para a criação das vagas em models

--- empresa/models.py
+++ empresa/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Tecnologias(models.Model):
+    tecnologia = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.tecnologia
+
 class Empresa(models.Model):
     choices_nicho_mercado = (
         ('MK', 'Marketing'),
@@ -16,11 +22,42 @@ class Empresa(models.Model):
         ('FM', 'Farmácia'),
         ('PR', 'Professor')
     )
+    logo = models.ImageField(upload_to="logo_empresa", null=True)
     nome = models.CharField(max_length=30)
     email = models.EmailField()
     cidade = models.CharField(max_length=30)
     endereço = models.CharField(max_length=30)
+    tecnologias = models.ManyToManyField(Tecnologias)
     carac_empresa = models.TextField()
     nicho_mercado = models.CharField(max_length=5, choices=choices_nicho_mercado)
     
+    def __str__(self):
+        return self.nome
+
+class Vagas(models.Model):
+    choices_experiencia = (
+        ('J', 'Júnior'),
+        ('P', 'Pleno'),
+        ('S', 'Sênior')
+    )
+
+    choices_status = (
+        ('I', 'Interesse'),
+        ('C', 'Currículo enviado'),
+        ('E', 'Entrevista'),
+        ('D', 'Desafio técnico'),
+        ('F', 'Finalizado')
+    )
+    
+    empresa = models.ForeignKey(Empresa, on_delete=models.DO_NOTHING)
+    titulo = models.CharField(max_length=30)
+    nivel_experiencia = models.CharField(max_length=2, choices=choices_experiencia)
+    data_final = models.DateField()
+    status = models.CharField(max_length=30, choices=choices_status)
+    tecnologias_dominadas = models.ManyToManyField(Tecnologias)
+    tecnologias_estudar = models.ManyToManyField(Tecnologias, related_name='estudar')
+
+
+    def __str__(self):
+        return self.titulo
     
