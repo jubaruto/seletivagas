3e1c7f468b23f42859a86dd39d52606956f5d6bc
Juliana Souto <barutotec@gmail.com>
2022-11-08 01:17:28 -0300

    Foram inseridos comandos no arquivo models

--- empresa/models.py
+++ empresa/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Empresa(models.Model):
+    choices_nicho_mercado = (
+        ('MK', 'Marketing'),
+        ('NT', 'Nutrição'),
+        ('DN', 'Design'),
+        ('PS', 'Psicologia'),
+        ('EC', 'Engenharia Civil'),
+        ('FN', 'Fonoaudiologia'),
+        ('EM', 'Engenharia Mecânica'),
+        ('EP', 'Engenharia de Produção'),
+        ('DS', 'Desenvolvedor de Software')
+        ('MT', 'Motorista'),
+        ('AS', 'Assitente Social'),
+        ('FM', 'Farmácia'),
+        ('PR', 'Professor')
+    )
+    nome = models.CharField(max_length=30)
+    email = models.EmailField()
+    cidade = models.CharField(max_length=30)
+    endereço = models.CharField(max_length=30)
+    carac_empresa = models.TextField()
+    nicho_mercado = models.CharField(max_length=3, choices=choices_nicho_mercado)
+    
+    
