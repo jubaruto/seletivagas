871ec5f5e2718c7303c3d4d73a79f249996ceff7
Juliana Souto <barutotec@gmail.com>
2022-11-10 22:09:19 -0300

    Foi implementado códigos

--- empresa/views.py
+++ empresa/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Tecnologias, Empresa
+from .models import Tecnologias, Empresa, Vagas
 from django.shortcuts import redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.messages import constants
@@ -42,7 +42,7 @@ def nova_empresa(request):
         empresa.tecnologias.add(*tecnologias)
         empresa.save()
         messages.add_message(request, constants.SUCCESS, 'Empresa cadastrada com sucesso')
-        return redirect('/home/empresas')
+        return redirect('/home/nova_empresas')
     
 def empresas(request):
     filtrar_tecnologias = request.GET.get('tecnologias')
@@ -66,7 +66,13 @@ def excluir_empresa(request, id):
     
 def empresa(request, id):
     empresa_unica = get_object_or_404(Empresa, id=id)
-    return render(request, 'empresa_unica.html', {'empresa': empresa_unica})
+    empresas = Empresa.objects.all()
+    tecnologias = Tecnologias.objects.all()
+    vagas = Vagas.objects.filter(empresa_id=id)
+    return render(request, 'empresa_unica.html', {'empresa': empresa_unica, 'tecnologias': tecnologias,
+                                                  'empresas': empresas,
+                                                  'vagas': vagas})
+    
 
 
 
