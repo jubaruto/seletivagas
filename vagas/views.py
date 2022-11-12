28a074c0285056620187962755ee672bb8a2344b
Juliana Souto <barutotec@gmail.com>
2022-11-12 18:14:15 -0300

    Foi criado nova tarefa em views

--- vagas/views.py
+++ vagas/views.py
@@ -1,9 +1,10 @@
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from empresa.models import Vagas
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.messages import constants
+from .models import Tarefa
 
 def nova_vaga(request):
     if request.method == "POST":
@@ -36,4 +37,28 @@ def nova_vaga(request):
         
     elif request.method == "GET":
         raise Http404()
+    
+    
+
+def vaga(request, id):
+    vaga = get_object_or_404(Vagas, id=id)
+    return render(request, 'vaga.html', {'vaga': vaga})
+    
+
+def nova_tarefa(request, id_vaga):
+    titulo = request.POST.get('titulo')
+    prioridade = request.POST.get("prioridade")
+    data = request.POST.get('data')
+    
+    try:
+        tarefa = Tarefa(vaga_id=id_vaga,
+                        titulo=titulo,
+                        prioridade=prioridade,
+                        data=data)
+        tarefa.save()
+        messages.add_message(request, constants.SUCCESS, 'Tarefa criada com sucesso')
+        return redirect(f'/vagas/vaga/{id_vaga}')
+    except:
+        messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+        return redirect(f'/vagas/vaga/{id_vaga}')
     
