cbeb1b081d153bb3707d4cb53b75a220caa2e133
Juliana Souto <barutotec@gmail.com>
2022-11-12 18:10:32 -0300

    Foi criado uma nova url para nova tarefa

--- vagas/urls.py
+++ vagas/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     
     path('nova_vaga/', views.nova_vaga, name="nova_vaga"),
     path('vaga/<int:id>', views.vaga, name="vaga"),
+    path('nova_tarefa/<int:id_vaga>', views.nova_tarefa, name="nova_tarefa"),
 ]
