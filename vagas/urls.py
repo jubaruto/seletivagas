48d930bae008454bf34f3c68ea2e12e8a86e8f52
Juliana Souto <barutotec@gmail.com>
2022-11-11 21:04:05 -0300

    Foi criada a url para que o usuário possa ser direcionado para o progresso da vaga selecionada

--- vagas/urls.py
+++ vagas/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     
-    path('nova_vaga/', views.nova_vaga, name="nova_vaga")
-    
+    path('nova_vaga/', views.nova_vaga, name="nova_vaga"),
+    path('vaga/<int:id>', views.vaga, name="vaga"),
 ]
