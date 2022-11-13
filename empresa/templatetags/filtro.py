32263f7a3138c851433ffcf83ad4d9202c10fe3c
Juliana Souto <barutotec@gmail.com>
2022-11-13 03:38:04 -0300

    Foram realizadas correções no código

--- empresa/templatetags/filtro.py
+++ empresa/templatetags/filtro.py
@@ -2,6 +2,6 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name="par")
-def par(valor):
+@register.filter(name="is_par")
+def is_par(valor):
     return True if valor % 2 == 0 else False
